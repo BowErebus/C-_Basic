f4c2c8c87031da88a1ea4cea29a7197a433b162f
Bow <voxuanphu9a4@gmail.com>
2022-05-18 16:36:31 +0700

    Basic_Import_and_Export

---
+++ Basic_Import_and_Export/Basic_Import_and_Export/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Import_and_Export
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Console.Write("");
+
+            //Console.Write("");
+            //Console.Write("Bow");
+            //Console.Write(10);
+            //Console.Write(10.5f);
+            //Console.Write(true);
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Console.WriteLine("");
+
+            //Console.Write("Bow \n");
+            //Console.WriteLine(10);
+            //Console.Write(10.5f);
+            //Console.Write(Environment.NewLine);
+            //Console.Write(true);
+
+            //Console.ReadLine();
+
+            //--------------------------------------------------------------------
+
+            //int a = 5;
+            //Console.Write("a = ");
+            //Console.WriteLine(a);
+
+            //int b = 6;
+            //Console.WriteLine("b = " + b);
+            ////Console.Write(b);
+
+            //int c = 7;
+            //Console.WriteLine("c = {0}", c);
+            ////Console.Write(c);
+            //Console.Write("{0} {1} {2} {3}", 26, 05, 02, "Bow");
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Console.Read();
+
+            //Console.WriteLine(Console.Read());
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Console.ReadLine();
+
+            //Console.WriteLine(Console.ReadLine());
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Console.ReadKey();
+
+            //Console.WriteLine("Bow");
+
+            //Console.ReadKey();
+            //Console.ReadKey(false);
+            //Console.ReadKey(true);
+
+            #endregion
+
+            #region Ex
+
+            //Console.WriteLine("         Bow");
+            //Console.Write(" Enter your name: ");      
+            //Console.WriteLine("Your name is: " + Console.ReadLine());
+            //Console.Write(" Enter your date of birth: ");
+            //Console.WriteLine(" Your date of birth is: " + Console.ReadLine());
+            //Console.Write(" Enter your home town: ");
+            //Console.WriteLine(" Home town: " + Console.ReadLine());
+
+            //Console.ReadKey();
+
+            #endregion
+
+        }
+    }
+}
