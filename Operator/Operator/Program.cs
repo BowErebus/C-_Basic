75c4f25b563a999d8d0a5d7d22237b5e585c5509
Bow <voxuanphu9a4@gmail.com>
2022-05-21 18:45:30 +0700

    Operator

---
+++ Operator/Operator/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Math Operator
+
+            //int i = 2, j = 7;
+
+            //Console.WriteLine(i++);
+            //Console.WriteLine(++j);
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Initialization And Assignment Operators
+
+            //int H, K, T;
+            //H = K = T = 10;
+            //Console.WriteLine("H = {0}, K = {1}, T = {2}", H, K, T);
+
+            //H += K = T = 5;
+            //Console.WriteLine("H = {0}, K = {1}, T = {2}", H, K, T);
+
+            //H = K += T = 5;
+            //Console.WriteLine("H = {0}, K = {1}, T = {2}", H, K, T);
+
+            //H += K += T = 5;
+            //Console.WriteLine("H = {0}, K = {1}, T = {2}", H, K, T);
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Ex1
+
+            //int a, b, c;
+
+            //a = b = (c = 9) + 1;
+            //a += b;
+            //b = c++;
+            //--c;
+
+            //Console.WriteLine(" a = {0}, b = {1}, c = {2}", a, b, c);
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Ex2
+
+            //string strInteger;
+            //int integer;
+            //string result;
+
+            //strInteger = console.readline();
+            //integer = int32.parse(strInteger);
+            //result = (integer % 2 == 0) ? "even" : "odd";
+
+            //console.writeline("{0} la {1}", integer, result);
+
+            //console.readkey();
+
+            #endregion
+        }
+    }
+}
