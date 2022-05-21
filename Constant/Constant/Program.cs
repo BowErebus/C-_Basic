ce49794b9c5b5c9bddc2f69e0dc63857d2fe9e03
Bow <voxuanphu9a4@gmail.com>
2022-05-21 18:48:34 +0700

    Constant

---
+++ Constant/Constant/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a = 15;
+            const int x = a;
+
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
