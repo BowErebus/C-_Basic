19b85dd82c7f8a83b42d1f45fe3b5fa642954fc8
Bow <voxuanphu9a4@gmail.com>
2022-07-06 14:58:42 +0700

    If_Else

---
+++ If_Else/If_Else/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Missing conditional sentences
+
+            //string B = "Bow";
+
+            //if (B == "Bow")
+            //    Console.WriteLine("Bow Erebus");
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Sufficient conditional sentence
+
+            //string B = "Bow";
+
+            //if (B == "Bow")
+            //{
+            //    Console.Write("Bow ");
+            //    Console.WriteLine("Erebus");
+            //}
+            //else if (B == "StrongBow")
+            //{
+            //    Console.WriteLine("Drink!");
+            //}
+            //else
+            //{
+            //    if (B == "26-05")
+            //    {
+            //        Console.WriteLine("Public website Bow Erebus");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Connecting StrongBow . . .");
+            //    }
+            //}
+            //    Console.ReadKey();
+
+            #endregion
+
+            #region Conditional operator
+
+            //string bowErebus = "BowErebus";
+            //bool isBowErebus = bowErebus == "BowErebus" ? true : false;
+
+            //if (isBowErebus)
+            //{
+            //    Console.WriteLine("Welcome to BowErebus");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("See You Again!");
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Ex
+
+            //string strA, strB;
+            //int A, B;
+            //double Root;
+
+            //Console.WriteLine("         ******************************************");
+            //Console.WriteLine("         *                                        *");
+            //Console.WriteLine("         *   Equation solver program Ax + B = 0   *");
+            //Console.WriteLine("         *                                        *");
+            //Console.WriteLine("         *******************************************");
+
+            //Console.Write("\n Please Enter The Number A: ");
+            //strA = Console.ReadLine();
+            //Console.Write("\n Please Enter The Number B: ");
+            //strB = Console.ReadLine();
+
+            //if (int.TryParse(strA, out A) == false || int.TryParse(strB, out B) == false)
+            //{
+            //    Console.WriteLine(" Wrong input data!"); 
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\n Your equation just entered is : {0}x + {1} = 0", A, B);
+            //    if (A == 0)
+            //    {
+            //        Console.WriteLine("Your equation have no solution!");
+            //    }
+            //    else if (B == 0)
+            //    {
+            //        Console.WriteLine("Your equation have solution x = 0");
+            //    }
+            //    else
+            //    {
+            //        Root = (double)-B / A;
+            //        Console.WriteLine("\n Your equation have solution x = {0}", Root);
+            //    }
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
