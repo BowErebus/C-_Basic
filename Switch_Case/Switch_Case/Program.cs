e6896d8cd5aea82a5ba1f4126988195987a111d8
Bow <voxuanphu9a4@gmail.com>
2022-07-06 15:00:37 +0700

    Switch_Case

---
+++ Switch_Case/Switch_Case/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Missing Switch
+
+            //int b = 10;
+
+            //switch (b)
+            //{
+            //    case 3:
+            //        Console.WriteLine("Bow Erebus");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Bow");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Bow Erebus . . .");
+            //        break;
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Sufficient Switch
+
+            //int b = 1;
+
+            //switch (b)
+            //{
+            //    case 3:
+            //        Console.WriteLine("Bow Erebus");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Bow");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Bow Erebus . . .");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Connecting to BowErebus . . .");
+            //        break;
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Ex
+
+            //int Year;
+            //string Can = "", Chi = "";
+
+            //Console.Write(" Please Enter Any Year : ");
+            //Year = Int32.Parse(Console.ReadLine());
+
+            //switch (Year % 10)
+            //{
+            //    case 0:
+            //        Can = "Canh";
+            //        break;
+            //    case 1:
+            //        Can = "Tan";
+            //        break;
+            //    case 2:
+            //        Can = "Nham";
+            //        break;
+            //    case 3:
+            //        Can = "Quy";
+            //        break;
+            //    case 4:
+            //        Can = "Giap";
+            //        break;
+            //    case 5:
+            //        Can = "At";
+            //        break;
+            //    case 6:
+            //        Can = "Binh";
+            //        break;
+            //    case 7:
+            //        Can = "Dinh";
+            //        break;
+            //    case 8:
+            //        Can = "Mau";
+            //        break;
+            //    case 9:
+            //        Can = "Ky";
+            //        break;
+            //}
+
+            //switch (Year % 12)
+            //{
+            //    case 0:
+            //        Chi = "Than";
+            //        break;
+            //    case 1:
+            //        Chi = "Dau";
+            //        break;
+            //    case 2:
+            //        Chi = "Tuat";
+            //        break;
+            //    case 3:
+            //        Chi = "Hoi";
+            //        break;
+            //    case 4:
+            //        Chi = "Ty";
+            //        break;
+            //    case 5:
+            //        Chi = "Suu";
+            //        break;
+            //    case 6:
+            //        Chi = "Dan";
+            //        break;
+            //    case 7:
+            //        Chi = "Meo";
+            //        break;
+            //    case 8:
+            //        Chi = "Thin";
+            //        break;
+            //    case 9:
+            //        Chi = "Ti";
+            //        break;
+            //    case 10:
+            //        Chi = "Ngo";
+            //        break;
+            //    case 11:
+            //        Chi = "Mui";
+            //        break;
+            //}
+
+            //Console.WriteLine("Year {0} is a lunar year is: {1} {2}", Year, Can, Chi);
+
+            //Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
