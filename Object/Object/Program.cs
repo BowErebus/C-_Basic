e3645eb5298cec8142056e0eb415db58857ff2bd
Bow <voxuanphu9a4@gmail.com>
2022-07-09 16:21:22 +0700

    Object

---
+++ Object/Object/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "BowErebus";
+
+            int value = 100;
+
+            object objectValue = value;
+
+            int newValue = (int)objectValue;
+
+            Console.ReadKey();
+        }
+    }
+}
