16ff9c834c1f3884c238c992fe2516a323e0398c
Bow <voxuanphu9a4@gmail.com>
2022-07-09 16:22:29 +0700

    Var

---
+++ Var/Var/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var varString = "Bow";
+
+            string content = "BowErebus";
+
+            Console.WriteLine(varString);
+            Console.WriteLine(content);
+
+            Console.ReadKey();
+        }
+    }
+}
