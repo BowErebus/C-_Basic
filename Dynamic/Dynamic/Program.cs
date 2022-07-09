d58c51aa7f9d3cc5657262ca0cedbeb77a02faab
Bow <voxuanphu9a4@gmail.com>
2022-07-09 16:23:35 +0700

    Dynamic

---
+++ Dynamic/Dynamic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Characteristics of dynamic type
+
+            //dynamic StringValue = "BowErebus";
+
+            //StringValue++;
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Program using dynamic
+
+            //string Name = "Bow";
+            //string Mission = "Erebus";
+
+            //dynamic DynamicName = Name;
+
+            //Console.WriteLine("Mission of " + DynamicName + " is " + Mission);
+
+            //Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
