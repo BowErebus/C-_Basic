9aa17c5b1d7bbd97bfcb7e8910e953f90e5714d9
Bow <voxuanphu9a4@gmail.com>
2022-07-20 15:55:58 +0700

    Structure_Of_The_Function

---
+++ Structure_Of_The_Function/Structure_Of_The_Function/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure_Of_The_Function
+{
+    class Program
+    {
+        #region Ex1
+
+        //static void Main(string[] args)
+        //{
+
+        //    Demo();
+
+        //    Console.ReadKey();
+        //}
+
+        //static int ReturnANumber()
+        //{
+        //    return 5;
+        //}
+
+        //static void Demo()
+        //{
+        //    Console.WriteLine(ReturnANumber());
+        //}
+
+        #endregion
+
+        #region Ex2
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(SumTwoNumber(5, 10));
+        //    Console.ReadKey(); 
+        //}
+
+        //static int SumTwoNumber(int firstNumber, int secondNumber)
+        //{
+        //    return firstNumber + secondNumber;
+        //}
+
+        #endregion
+
+        #region Ex3
+
+        //static void Main(string[] args)
+        //{
+        //    int firstNumber = 0;
+        //    int secondNumber = 3;
+        //    for (int count = 0; count < 10; count++)
+        //    {
+        //        PrintSumTwoNumber(firstNumber, secondNumber);
+        //        firstNumber += count;
+        //        secondNumber += count * 2 / 5;
+        //    }
+        //    Console.ReadKey();
+        //}
+
+        //static void PrintSumTwoNumber(int firstNumber, int secondNumber)
+        //{
+        //    Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, SumTwoNumber(firstNumber, secondNumber));
+        //}
+
+        //static int SumTwoNumber(int firstNumber, int secondNumber)
+        //{
+        //    return firstNumber + secondNumber;
+        //}
+
+        #endregion
+
+        #region Ex4
+
+        //static void Main(string[] args)
+        //{
+        //    PrintSomeThing("2002", 20);
+        //    PrintSomeThing("BowErebus", 1);
+        //    Console.ReadKey();
+        //}
+
+        //static void PrintSomeThing(string name, int age)
+        //{
+        //    Console.WriteLine("This is {0}, {1} years old.", name, age);
+        //}
+
+        #endregion
+    }
+}
