57c2a054f2920598cb3207ef3159be982e746663
Bow <voxuanphu9a4@gmail.com>
2022-07-23 17:06:41 +0700

    Ref_And_Out_Keywords

---
+++ Ref_And_Out_Keywords/Ref_And_Out_Keywords/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_And_Out_Keywords
+{
+    class Program
+    {
+        #region Ref
+
+        #region Ex1
+
+        //static void Main(string[] args)
+        //{
+        //    int value = 5;
+
+        //    Console.WriteLine("Value before increase: {0}", value);
+
+        //    IncreaseValue(value);
+
+        //    Console.WriteLine("Value after increase: {0}", value);
+
+        //    Console.ReadKey();
+        //}
+
+        //static void IncreaseValue(int value)
+        //{
+        //    value++;
+        //}
+
+        #endregion
+
+        #region Ex2
+
+        //static void Main(string[] args)
+        //{
+        //    int value = 5;
+
+        //    Console.WriteLine("value before increase: {0}", value);
+
+        //    increasevalue(ref value);
+
+        //    Console.WriteLine("value after increase: {0}", value);
+
+        //    Console.ReadKey();
+        //}
+
+        //static void increasevalue(ref int value)
+        //{
+        //    value++;
+        //}
+
+        #endregion
+
+        #endregion
+
+        #region Out
+
+        #region Ex
+
+        //static void Main(string[] args)
+        //{
+        //    int value = 5;
+
+        //    Console.WriteLine("Value before increase: {0}", value);
+
+        //    IncreaseValue(out value);
+
+        //    Console.WriteLine("Value after increase: {0}", value);
+
+        //    Console.ReadKey();
+        //}
+
+        //static void IncreaseValue(out int value)
+        //{
+        //    value = 0;
+
+        //    value++;
+        //}
+
+        #endregion
+
+        #endregion
+    }
+}
