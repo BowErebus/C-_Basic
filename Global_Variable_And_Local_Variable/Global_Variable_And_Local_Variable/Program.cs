059498347e5b591612335dd51987ab5788dbb3db
Bow <voxuanphu9a4@gmail.com>
2022-07-23 17:04:59 +0700

    Global_Variable_And_Local_Variable

---
+++ Global_Variable_And_Local_Variable/Global_Variable_And_Local_Variable/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Global_Variable_And_Local_Variable
+{
+    class Program
+    {
+        #region Ex1
+
+        //static int value = 5;
+
+        //static void Main(string[] args)
+
+        //{
+
+        //    Console.WriteLine(value);
+
+        //    value = 10;
+
+        //    PrintSomeThing();
+
+        //    Console.ReadKey();
+        //}
+
+
+
+        //static void PrintSomeThing()
+
+        //{
+        //    int value = 9;
+
+        //    Console.WriteLine(value);
+        //}
+
+        #endregion
+
+        #region Ex2
+
+        //static int value = 5;
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(value);
+
+        //    value = 10;
+
+        //    PrintSomeThing();
+
+        //    Console.ReadKey();
+        //}
+
+        //static void PrintSomeThing()
+        //{
+        //    Console.WriteLine(value);
+        //}
+
+        #endregion
+
+        #region Ex3
+
+        //static int value = 5;
+
+        //static void Main(string[] args)
+
+        //{
+        //    Console.WriteLine(value);
+
+        //    value = 10;
+
+        //    PrintSomeThing();
+
+        //    Console.ReadKey();
+        //}
+
+        //static void PrintSomeThing()
+        //{
+        //    int value = 9;
+
+        //    Console.WriteLine(value);
+        //}
+
+        #endregion
+
+        #region Ex4
+
+        //static void Main(string[] args)
+        //{
+        //    bool isTrue = true;
+
+        //    if (isTrue)
+        //    {
+        //        int value = 5;
+
+        //        Console.WriteLine(value);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(value);
+        //    }
+
+        //    Console.ReadKey();
+        //}
+
+        #endregion
+    }
+}
